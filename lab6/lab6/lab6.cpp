1116ab967e5101b25c11695945aabcc116faa089
Anton Syski <antonsyski@gmail.com>
2019-03-12 14:38:56 +0300

    add ~~var5

--- lab6/lab6/lab6.cpp
+++ lab6/lab6/lab6.cpp
@@ -6,10 +6,13 @@ using namespace std;
 
 void var1();
 void var2();
+void var3();
+void var4();
+void var5();
 
 int main()
 {
-	var2();
+	var5();
 }
 
 void var1()
@@ -64,14 +67,14 @@ void var2()
 	}
 	for (; counterBinarString < strlen(binarString); counterBinarString++)
 	{
-		if (binarString[counterBinarString - 1] == binarString[counterBinarString] || 
-			binarString[counterBinarString + 1] == binarString[counterBinarString] )
+		if (binarString[counterBinarString - 1] == binarString[counterBinarString] ||
+			binarString[counterBinarString + 1] == binarString[counterBinarString] ||
+			binarString[counterBinarString + 1] == '\0')
 		{
 			mirrorMinBinarString[counterMinBinarString] = binarString[counterBinarString];
 			counterMinBinarString++;
 		}
-		if (binarString[counterBinarString - 1] != binarString[counterBinarString] ||
-			binarString[counterBinarString + 1] != binarString[counterBinarString])
+		if (binarString[counterBinarString + 1] != binarString[counterBinarString])
 		{
 			mirrorMinBinarString[counterMinBinarString] = '\0';
 			if (strlen(mirrorMinBinarString) <= strlen(minBinarString))
@@ -82,4 +85,111 @@ void var2()
 		}
 	}
 	puts(minBinarString);
+}
+
+void var3()
+{
+	char binarString[100], maxBinarString[100], mirrorMinBinarString[100];
+	int counterMinBinarString = 0, counterBinarString = 0;
+
+	cout << "Enter your binar string with out spaces" << endl;
+	gets_s(binarString);
+
+	for (; counterBinarString < strlen(binarString); counterBinarString++)
+	{
+		if (binarString[counterBinarString - 1] == binarString[counterBinarString] || counterBinarString == 0)
+		{
+			maxBinarString[counterMinBinarString] = binarString[counterMinBinarString];
+			counterMinBinarString++;
+		}
+		else
+		{
+			maxBinarString[counterMinBinarString] = '\0';
+			counterMinBinarString = 0;
+			break;
+		}
+	}
+	for (; counterBinarString < strlen(binarString); counterBinarString++)
+	{
+		if (binarString[counterBinarString - 1] == binarString[counterBinarString] ||
+			binarString[counterBinarString + 1] == binarString[counterBinarString] ||
+			binarString[counterBinarString + 1] == '\0')
+		{
+			mirrorMinBinarString[counterMinBinarString] = binarString[counterBinarString];
+			counterMinBinarString++;
+		}
+		if (binarString[counterBinarString + 1] != binarString[counterBinarString])
+		{
+			mirrorMinBinarString[counterMinBinarString] = '\0';
+			if (strlen(mirrorMinBinarString) >= strlen(maxBinarString))
+			{
+				strcpy_s(maxBinarString, mirrorMinBinarString);
+			}
+			counterMinBinarString = 0;
+		}
+	}
+	cout << strlen(maxBinarString);
+}
+
+void var4()
+{
+	char binarString[100], gproupString[100];
+	int counterGroupString = 0;
+
+	cout << "Enter your binar string with out spaces" << endl;
+	gets_s(binarString);
+
+	for (int counterBinarString = 0; counterBinarString < strlen(binarString); counterBinarString++)
+	{
+		if (binarString[counterBinarString - 1] == binarString[counterBinarString] ||
+			binarString[counterBinarString + 1] == binarString[counterBinarString] ||
+			binarString[counterBinarString + 1] == '\0')
+		{
+			gproupString[counterGroupString] = binarString[counterBinarString];
+			counterGroupString++;
+		}
+		if (binarString[counterBinarString + 1] != binarString[counterBinarString])
+		{
+			gproupString[counterGroupString] = '\0';
+			if (strlen(gproupString)% 2 == 0) 
+			{
+				puts(gproupString);
+			}
+			counterGroupString = 0;
+		}
+	}
+}
+
+void var5()
+{
+	char binarString[100], groupString[100];
+	int counterGroupString = 0, counterOfOne = 0;
+
+	cout << "Enter your string" << endl;
+	gets_s(binarString);
+
+	for (int counterBinarString = 0; counterBinarString <= strlen(binarString); counterBinarString++)
+	{
+		if (binarString[counterBinarString] != ' ')
+		{
+			groupString[counterGroupString] = binarString[counterBinarString];
+			counterGroupString++;
+		}
+		if(binarString[counterBinarString] == ' ' || binarString[counterBinarString] == '\0')
+		{
+			groupString[counterGroupString] = '\0';
+			counterGroupString = 0;
+			if (strlen(groupString) % 2 != 0)
+			{
+				for (int counterSubgroup = 0; counterSubgroup < strlen(groupString); counterSubgroup++)
+				{
+					if (groupString[counterSubgroup] == '1')
+					{
+						counterOfOne++;
+					}
+				}
+			}
+		}
+	}
+	cout << counterOfOne;
 }
